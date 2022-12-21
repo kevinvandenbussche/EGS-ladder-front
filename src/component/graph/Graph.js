1b85321c499487622159699eef361346a7556d25
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2022-12-21 09:54:08 +0100

    creation du composant graph

---
+++ src/component/graph/Graph.js
@@ -0,0 +1,22 @@
+
+export function Graph(){
+    const entrypoint = 'http://localhost:8000/';
+    const [page, setPage] = useState(1);
+    const [users, setUsers] = useState([]);
+    useEffect(() =>{
+        const url = entrypoint + "api/users/1";
+        fetch( url ,   { headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }})
+        .then(res => res.json())
+        .then(
+            (result) =>{
+                //thread operator
+                let arrayData = [...users, ...result];
+                setUsers(arrayData); 
+            }
+        )
+        
+    }, [page]);
+}
