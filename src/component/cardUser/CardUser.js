6488e76c6e4579fae11a7b3ade603641bc1603d5
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2022-12-21 22:07:09 +0100

    creation du composant card et gestion de la pagination

--- src/component/cardUser/CardUser.js
+++ src/component/cardUser/CardUser.js
@@ -1,13 +1,17 @@
 import './CardUser.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
+
 
 export function CardUser(){
     const entrypoint = 'http://localhost:8000/';
+    const entrypointForToPlay = 'http://localhost:8000';
+
     const [page, setPage] = useState(1);
     const [users, setUsers] = useState([]);
+    const [pseudo, setPseudo] = useState([]);
     
     useEffect(() =>{
-        const url = entrypoint + "api/users?page=" + page;
+        const url = entrypoint + 'api/data-user-for-main-page';
         fetch( url ,   { headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -17,27 +21,31 @@ export function CardUser(){
             (result) =>{
                 //thread operator
                 let arrayData = [...users, ...result];
-                setUsers(arrayData);
+                setUsers(result);
                 if(result.length === 30){
                     let pageCopy = page;
-                    setPage(pageCopy += 1);                    
-                }  
+                    setPage(pageCopy += 1); 
+                }
             }
+           
         )
         
-    }, [page]);
-    console.log(users);
+    }, [page]);    
+    
+
+    console.log(users)
+
     return(
         <div className='position-relative-for-other-element'>
             <h1 className='title-player text-align-center'>suivi de tous les joueurs</h1>
             <div className='flex wrap'>
                 {users.map((user)=>{
                         return(
-                        <div className='flex card-user space-arround align-items-center'>
+                        <div key={user.id} className='flex card-user space-arround align-items-center'>
                             <div>
-                                <p>prenom: <span> {user.firstname}</span></p>
-                                <p>nom: <span> {user.name}</span></p>
-                                <p>pseudo:</p>
+                                <p>prenom: <span>&nbsp;{user.firstname}</span></p>
+                                <p>nom: <span>&nbsp;{user.name}</span></p>
+                                <p>pseudo: <span>&nbsp;{user.pseudonyme}</span></p>
                                 <p>jeu:</p>
                             </div>
                             <div className='flex space-arround '>
