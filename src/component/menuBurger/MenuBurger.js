8707f89ceebfabc85508fdaf5a678c9b0eedea09
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-01-10 17:25:45 +0100

    ajout du composant menu bruger

---
+++ src/component/menuBurger/MenuBurger.js
@@ -0,0 +1,12 @@
+import './menuBurger.scss'
+
+export function MenuBurger(props){
+    console.log(props)
+    return(
+        <ul className='menu-burger'>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+
+}
