e5b327ef03698e185033cc7d46d2f75306d94942
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-01-11 13:42:41 +0100

    ajout du loader

---
+++ src/component/load/Load.js
@@ -0,0 +1,9 @@
+import './load.scss'
+export function Load(){
+    return(
+        <div className='load'>
+            <h1>chargement</h1>
+            <div class="loader"></div>
+        </div>
+    )
+}
