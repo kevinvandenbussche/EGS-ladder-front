0a7efaec168073765b7f0686602839b3ee832fa0
kevinvandenbussche <kevin.vandenbussche.pro@gmail.com>
2023-04-06 15:27:33 +0200

    redirection des users lors d'une 404

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import {Logo3d} from './component/logo/Logo3d.js';
 import { CardUser } from './component/cardUser/CardUser.js';
 import { Graph } from './component/graph/Graph.js';
 import { FormUser } from './component/formUser/FromUser.js';
-import { EditUserForm } from './component/editUserForm/EditUserForm.js';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter  as Router, Route, Routes } from "react-router-dom";
 import { SelectGame } from './component/selectGame/SelectGame.js';
 import { Login } from './component/login/Login.js';
+import { Message } from './component/message/Message.js';
 import './asset/_generic.scss';
 
 
@@ -23,6 +23,8 @@ function App() {
           <Route path="/select-game/:id" element={<SelectGame/>} />
           <Route path="/" element={<Login />} />
           <Route path="/create-account" element={<FormUser/>} />
+          <Route path="message" element={<Message/>} />
+          <Route path='*' element={<Login />}/>
         </Routes>
       </Router>
       </div>
